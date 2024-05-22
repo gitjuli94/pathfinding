a412f266865dbeaf5281764144974390a82fb8e1
Julia Rahkonen <juliarahkonen1@gmail.com>
2024-05-22 18:53:44 +0200

    maps added

---
+++ additional_scripts/maptolist.py
@@ -0,0 +1,32 @@
+def process_file(input_file_path, output_file_path):
+    try:
+        with open(input_file_path, 'r') as input_file:
+            lines = input_file.readlines()
+
+        result_array = []
+        for line in lines:
+            # Process the line to remove spaces and convert characters
+            result_line = []
+            for char in line:
+                if char not in [" ", "\n"]:
+                    if char == '.':
+                        result_line.append("0")
+                    elif char == '@':
+                        result_line.append("1")
+            result_array.append(result_line)
+
+        # Write the processed map to the output file
+        with open(output_file_path, 'w') as output_file:
+            for line in result_array:
+                output_file.write(",".join(line) + "\n")
+
+        print(f"Map Processed, array written to {output_file_path}")
+
+    except FileNotFoundError:
+        print(f"Error: File not found - {input_file_path}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+input_file_path = 'Shanghai_256.map'
+output_file_path = 'output.txt'
+process_file(input_file_path, output_file_path)
